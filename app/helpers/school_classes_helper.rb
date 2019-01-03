2f0380be975f63f04d78d589dbf86588ec93c098
Judy McGoldrick <jjeanius@verizon.net>
2019-01-03 22:38:00 +0000

    Automatically backed up by Learn

--- app/helpers/school_classes_helper.rb
+++ app/helpers/school_classes_helper.rb
@@ -1,8 +1,8 @@
 require 'pry'
 module SchoolClassesHelper
 
-  def concat_strings(strings)
-     new_arr = strings.split("","")
+  def concat_strings(string1, string2)
+     new_arr = strings.split("")
      binding.pry
   end
 
